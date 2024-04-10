a3b3079ea689ce524d8ee3beb151579bc26ce881
AlianBlank <wangfj11@foxmail.com>
2024-04-10 17:44:53 +0800

    [增加]1. 增加Localization 本地化组件

---
+++ Runtime/GameFrameXLocalizationCroppingHelper.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace GameFrameX.Localization.Runtime
+{
+    public class GameFrameXLocalizationCroppingHelper : MonoBehaviour
+    {
+        private void Start()
+        {
+            _ = typeof(ILocalizationHelper);
+            _ = typeof(ILocalizationManager);
+            _ = typeof(Language);
+            _ = typeof(LoadDictionaryFailureEventArgs);
+            _ = typeof(LoadDictionarySuccessEventArgs);
+            _ = typeof(LoadDictionaryUpdateEventArgs);
+            _ = typeof(LocalizationManager);
+            _ = typeof(DefaultLocalizationHelper);
+        }
+    }
+}
